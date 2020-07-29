29fac9d9a5f454ad3eb878d269777ebc5523bd84
James Rawlings <rawlingsj80@gmail.com>
2020-07-29 16:01:17 +0100

    chore: add some basic reporting

--- go.mod
+++ go.mod
@@ -3,6 +3,17 @@ module github.com/jenkins-x/jx-kcheck
 go 1.14
 
 require (
-	github.com/jenkins-x/jx-kube-client v0.0.0-20200424124547-c43e4905869e // indirect
+	github.com/Comcast/kuberhealthy/v2 v2.1.2
 	github.com/jenkins-x/jx-logging v0.0.3
+	github.com/json-iterator/go v1.1.5 // indirect
+	github.com/spf13/pflag v1.0.3 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	k8s.io/client-go v10.0.0+incompatible // indirect
+)
+
+replace (
+	k8s.io/api => k8s.io/api v0.0.0-20190819141258-3544db3b9e44
+	k8s.io/apimachinery => k8s.io/apimachinery v0.0.0-20190817020851-f2f3a405f61d
+	k8s.io/client-go => k8s.io/client-go v0.0.0-20190819141724-e14f31a72a77
+
 )
--- main.go
+++ main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"github.com/Comcast/kuberhealthy/v2/pkg/checks/external/checkclient"
 	"github.com/jenkins-x/jx-logging/pkg/log"
 )
 
-// Entrypoint for the command
 func main() {
-	log.Logger().Infof("hey")
+
+	log.Logger().Infof("starting Jenkins X health checks")
+
+	errors := []string{}
+	// ingress
+
+	// tls
+
+	// dns
+
+	var err error
+	if len(errors) == 0 {
+		err = checkclient.ReportSuccess()
+		if err != nil {
+			log.Logger().Fatalf("failed to report success status %v", err)
+		}
+
+	} else {
+		err = checkclient.ReportFailure([]string{"a bad thing happened", "and another!"})
+		if err != nil {
+			log.Logger().Fatalf("failed to report failure status %v", err)
+		}
+	}
+
+	log.Logger().Infof("successfully reported")
 }
